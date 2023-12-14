5a15225f5ce027743a04a57ef8c1b8430861fa29
Azz <azz@aciedo.com>
2023-12-14 14:34:34 +0000

    dont segfault

--- game/src/game/gameover.rs
+++ game/src/game/gameover.rs
@@ -1,5 +1,6 @@
 use crate::game::aliens::ForAnyAlien;
 use crate::game::ships::PlayerShip;
+use bevy::app::AppExit;
 use bevy::prelude::*;
 
 use super::scoreboard::Score;
@@ -8,12 +9,13 @@ pub fn game_over_sys(
     aliens: Query<&Transform, ForAnyAlien>,
     ships: Query<&Transform, With<PlayerShip>>,
     score: Res<Score>,
+    mut exit: EventWriter<AppExit>,
 ) {
     let ship_pos = ships.get_single().unwrap().translation;
     for alien_pos in aliens.iter() {
         if alien_pos.translation.y < ship_pos.y {
             println!("Game Over - score {}", score.0);
-            std::process::exit(0);
+            exit.send(AppExit);
         }
     }
 }
